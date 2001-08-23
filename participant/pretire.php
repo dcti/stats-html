841a3cce4fa85dd40029c0a615a024c9189fd4d2
bwilson <bwilson>
2001-08-23 00:46:40 +0000

    Added 30-day versions of rc5 phistory, improved instructions for retire

--- participant/pretire.php
+++ participant/pretire.php
@@ -1,11 +1,11 @@
 <?php
-  // $Id: pretire.php,v 1.13 2000/11/15 14:43:34 decibel Exp $
+  // $Id: pretire.php,v 1.14 2001/08/23 00:46:40 bwilson Exp $
 
   // Parameters passed to pretire.php3
   // id = id to be retired
   // pass = password of id to be retired
   //
-  // em = EMAIL search string  \
+  // em = email search string  \
   //                            - either em or destid may be passed
   // destid = id to retire to  /
 
@@ -21,35 +21,35 @@
     print "
 	  <h2>You are about to permanently retire the address $par->EMAIL</h2>
 	  <p>
-	   You are about to completely and permanently remove this EMAIL from the stats database.
-	   All past, current, and future work submitted by this EMAIL will be attributed to a new
-	   EMAIL address.  This procedure is irreversable.  It is permanent.  Once you do this, we
+	   You are about to completely and permanently remove this email from the stats database.
+	   All past, current, and future work submitted by this email will be attributed to a new
+	   email address.  This procedure is irreversable.  It is permanent.  Once you do this, we
 	   cannot restore it.
 	  </p>
 	  <p>
 	   You should <strong>only</strong> be doing this if you will no longer be using the old
-	   EMAIL address.  This feature has been designed to allow participants to gracefully
-	   migrate from one EMAIL address to another without losing their longevity or standing
+	   email address.  This feature has been designed to allow participants to gracefully
+	   migrate from one email address to another without losing their longevity or standing
 	   in the stats database.
 	  </p>
 	  <p>
-	   In order to retire this EMAIL address, you must first pick a destination EMAIL address.
+	   In order to retire this email address, you must first pick a destination email address.
 	   All past blocks, and any future blocks that are submitted from $par->EMAIL will be
-	   treated as if they were submitted by the new EMAIL address you are about to choose.
+	   treated as if they were submitted by the new email address you are about to choose.
 	  </p>
 	  <p>
-	   Before you can retire this EMAIL address, you must have first changed over your clients
-	   to your new EMAIL address, and you must have flushed blocks from the new address and have
+	   Before you can retire this email address, you must have first changed over your clients
+	   to your new email address, and you must have flushed blocks from the new address and have
 	   those blocks visible in stats.
 	  </p>
 	  <p>
-	   Please enter your new EMAIL address below:
+	   Please enter your new email address below:
 	   <br>
 	   <form action=\"/pretire.php3\" method=\"post\">
 	    <input type=\"text\" name=\"ems\" size=\"64\" maxlength=\"64\">
 	    <input type=\"hidden\" name=\"id\" value=\"$id\">
 	    <input type=\"hidden\" name=\"pass\" value=\"$pass\">
-	    <input type=\"submit\" value=\"Search for this EMAIL\">
+	    <input type=\"submit\" value=\"Search for this email\">
 	   </form>
 	  </p>";
   } else {
@@ -65,16 +65,16 @@
       $matches = sybase_num_rows($result);
 
       print "
-	  <h2>Please choose your new EMAIL address</h2>
+	  <h2>Please choose your new email address</h2>
 	  <p>
-	   Below are all the EMAIL addresses in stats that match what you just typed in.
-	   Please choose the appropriate EMAIL address by clicking on it.
+	   Below are all the email addresses in stats that match what you just typed in.
+	   Please choose the appropriate email address by clicking on it.
 	  </p>
 	  <p>
 	   This <strong>will retire</strong> $par->EMAIL.
 	  </p>
 	  <p>
-	   Clicking an EMAIL below will result in a permanent change to the stats database.
+	   Clicking an email below will result in a permanent change to the stats database.
 	  </p>
 	  <table border=\"1\">
 	   <tr bgcolor=\"#00aaaa\">
@@ -116,14 +116,14 @@
 	  <p>That's cute, but it won't work.</p>";
         exit;
       }
-      $qs = "select count(*) as numEMAILs from STATS_participant where retire_to = $destid or retire_to = $id";
+      $qs = "select count(*) as numemails from STATS_participant where retire_to = $destid or retire_to = $id";
       $result = sybase_query($qs);
       $par = sybase_fetch_object($result);
-      if ( $par->numEMAILs >= 8 ) {
+      if ( $par->numemails >= 8 ) {
         print "
-	  <h2>Error: Too many retires to that EMAIL</h2>
-	  <p>To prevent abuse of the this facility, there is a limit of 8 EMAIL addresses which may be retired by one person.
-	     If there is a legitimate need for you to retire more than 8 EMAIL addresses, please contact help@distributed.net</p>";
+	  <h2>Error: Too many retires to that email</h2>
+	  <p>To prevent abuse of the this facility, there is a limit of 8 email addresses which may be retired into a single participant using this form.</p>
+	  <p>If you have a legitimate need to retire more than 8 participants, please send mail to help@distributed.net from the account you wish to retire specifying the participant you want it retired into and the circumstances of your request.  After investigating your request we may, at our discretion, retire the address for you.</p>";
         exit;
       }
       $qs = "update STATS_participant set retire_to = $destid, team = $destteam, retire_date = '" .
@@ -150,7 +150,7 @@
       print "
 	<h2>Retire Procedure successful</h2>
 	<p>
-	 You have successfully retired the EMAIL address $srcpar->EMAIL.
+	 You have successfully retired the email address $srcpar->EMAIL.
 	</p>
 	<p>
 	 This will take effect during the next stats run.
@@ -160,9 +160,9 @@
 	</p>
 	<p>
 	 All future blocks from this address will be attributed to team $destteam, which is your current team.
-	 If, in the future, you change teams, it will affect all retired EMAILs as you'd expect it to.
+	 If, in the future, you change teams, it will affect all retired emails as you'd expect it to.
 	</p>
-	<p>The EMAIL address $destpar->EMAIL currently has $rows EMAIL address$plural retired into it:</p>
+	<p>The email address $destpar->EMAIL currently has $rows email address$plural retired into it:</p>
 	<ul>";
       for($i = 0; $i<$rows; $i++) {
         sybase_data_seek($result,$i);
