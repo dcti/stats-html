b044fb45d316659c9603984d54bff846227389f6
paul <paul>
2002-04-10 22:09:52 +0000

    add projects.php

---
+++ projects.php
@@ -0,0 +1,21 @@
+<?
+
+ include "etc/config.inc";
+ include "etc/modules.inc";
+ include "etc/project.inc";
+
+
+ $title = "Overall Project Stats";
+
+ include "templates/header.inc";
+ if( file_exists("cache/index_$project_id.inc")) {
+	readfile( "cache/index_$project_id.inc");
+ } else {
+  ?> 
+	<center>	
+	Apologies, these pages are currently being built. Please try again in a few minutes
+	</center>
+  <? 
+}
+?>
+<?include "templates/footer.inc";?>
