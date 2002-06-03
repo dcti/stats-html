02b66a5ccd2cdfa44315209459572bc185bb10fa
paul <paul>
2002-06-03 17:32:17 +0000

    static.sh updated for new codebase

--- static.sh
+++ static.sh
@@ -1,11 +1,22 @@
 #!/bin/sh
 
 do_wget ( ) {
-	wget -O${2} "http://stats-${USER}.distributed.net/generic/${1}"
+	wget -O${2} "http://stats-${USER}.distributed.net/${1}"
 	return
 }
 
-do_wget pc_index.php index.html
-do_wget pc_countries.php countries.html
+do_wget pc_index.php?project_id=24 cache/index_24.inc
+do_wget pc_index.php?project_id=25 cache/index_25.inc
+do_wget pc_index.php?project_id=5 cache/index_5.inc
+
+do_wget misc/pc_countries.php?project_id=24\&source=o cache/countries_o_24.inc
+do_wget misc/pc_countries.php?project_id=25\&source=o cache/countries_o_25.inc
+do_wget misc/pc_countries.php?project_id=5\&source=o cache/countries_o_5.inc 
+do_wget misc/pc_countries.php?project_id=24\&source=y cache/countries_y_24.inc
+do_wget misc/pc_countries.php?project_id=25\&source=y cache/countries_y_25.inc
+do_wget misc/pc_countries.php?project_id=5\&source=y cache/countries_y_5.inc
+
+#do_wget pc_index.php index.html
+#do_wget pc_countries.php countries.html
 #do_wget pc_cpu.php cpu.html
 #do_wget pc_money.php money.html
