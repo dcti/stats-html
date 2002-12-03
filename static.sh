175a9f469fbcb5b10df6a9919898b846afa1285c
decibel <decibel>
2002-12-03 18:52:04 +0000

    Welocme rc5-72

--- static.sh
+++ static.sh
@@ -8,13 +8,16 @@ do_wget ( ) {
 do_wget pc_index.php?project_id=24 cache/index_24.inc
 do_wget pc_index.php?project_id=25 cache/index_25.inc
 do_wget pc_index.php?project_id=5 cache/index_5.inc
+do_wget pc_index.php?project_id=8 cache/index_8.inc
 
 do_wget misc/pc_countries.php?project_id=24\&source=o cache/countries_o_24.inc
 do_wget misc/pc_countries.php?project_id=25\&source=o cache/countries_o_25.inc
 do_wget misc/pc_countries.php?project_id=5\&source=o cache/countries_o_5.inc 
+do_wget misc/pc_countries.php?project_id=8\&source=o cache/countries_o_8.inc 
 do_wget misc/pc_countries.php?project_id=24\&source=y cache/countries_y_24.inc
 do_wget misc/pc_countries.php?project_id=25\&source=y cache/countries_y_25.inc
 do_wget misc/pc_countries.php?project_id=5\&source=y cache/countries_y_5.inc
+do_wget misc/pc_countries.php?project_id=8\&source=y cache/countries_y_8.inc
 
 #do_wget pc_index.php index.html
 #do_wget pc_countries.php countries.html
