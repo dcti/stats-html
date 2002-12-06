08b61e998b12a4efdeb80dd4d215de8a095d0933
decibel <decibel>
2002-12-06 13:43:40 +0000

    Whitespace

--- pc_index.php
+++ pc_index.php
@@ -1,76 +1,76 @@
 <?
+  # vi: ts=2 sw=2 tw=120 syntax=php
+  # $Id: pc_index.php,v 1.8 2002/12/06 13:43:40 decibel Exp $
 
- # $Id: pc_index.php,v 1.7 2002/06/03 13:32:00 paul Exp $
+  $title = "Overall Project Stats";
 
- $title = "Overall Project Stats";
+  include "etc/config.inc";
+  include "etc/modules.inc";
+  include "etc/project.inc";
 
- include "etc/config.inc";
- include "etc/modules.inc";
- include "etc/project.inc";
+  $qs = "select * from daily_summary where project_id=$project_id and date = (select max(date) from daily_summary where project_id=$project_id)";
+  $result = sybase_query($qs);
+  sybase_data_seek($result,0);
+  $daysum = sybase_fetch_object($result);
+  $lastupdate = sybase_date_format_long($daysum->DATE);
+  display_last_update('i');
 
- $qs = "select * from daily_summary where project_id=$project_id and date = (select max(date) from daily_summary where project_id=$project_id)";
- $result = sybase_query($qs);
- sybase_data_seek($result,0);
- $daysum = sybase_fetch_object($result);
- $lastupdate = sybase_date_format_long($daysum->DATE);
- display_last_update('i');
+  debug_text("<!-- Daily Summary -- qs: $qs, daysum: $daysum,  -->\n",$debug);
 
- debug_text("<!-- Daily Summary -- qs: $qs, daysum: $daysum,  -->\n",$debug);
+  $yest_work_units = number_format( (double) $daysum->WORK_UNITS/$proj_divider);
+  $yest_emails = number_format($daysum->PARTICIPANTS);
+  $yest_teams = number_format($daysum->TEAMS);
+  $new_emails = number_format($daysum->PARTICIPANTS_NEW);
+  $new_teams = number_format($daysum->TEAMS_NEW);
 
- $yest_work_units = number_format( (double) $daysum->WORK_UNITS/$proj_divider);
- $yest_emails = number_format($daysum->PARTICIPANTS);
- $yest_teams = number_format($daysum->TEAMS);
- $new_emails = number_format($daysum->PARTICIPANTS_NEW);
- $new_teams = number_format($daysum->TEAMS_NEW);
+  sybase_query("set rowcount 0");
+  $qs = "select sum(WORK_UNITS)/$proj_divider as TOT_UNITS, datediff(dd,min(date),max(date))+1 as time_working
+  from Daily_Summary
+  where PROJECT_ID=$project_id";
+  $result = sybase_query($qs);
+  sybase_data_seek($result,0);
+  $par = sybase_fetch_object($result);
 
- sybase_query("set rowcount 0");
- $qs = "select sum(WORK_UNITS)/$proj_divider as TOT_UNITS, datediff(dd,min(date),max(date))+1 as time_working
-	from Daily_Summary
-	where PROJECT_ID=$project_id";
- $result = sybase_query($qs);
- sybase_data_seek($result,0);
- $par = sybase_fetch_object($result);
+  debug_text("<!-- Time Working -- qs: $qs, result: $result, par: $par -->\n",$debug);
 
- debug_text("<!-- Time Working -- qs: $qs, result: $result, par: $par -->\n",$debug);
+  $time_working_raw = $par->time_working;
+  $time_working = number_format($par->time_working);
+  $tot_work_units = number_format( (double) $par->TOT_UNITS);
+  $TOT_UNITS = $par->TOT_UNITS;
 
- $time_working_raw = $par->time_working;
- $time_working = number_format($par->time_working);
- $tot_work_units = number_format( (double) $par->TOT_UNITS);
- $TOT_UNITS = $par->TOT_UNITS;
+  $qs = "select convert(char(8),count(*)) as emails from Email_Rank where PROJECT_ID=$project_id";
+  $result = sybase_query($qs);
+  sybase_data_seek($result,0);
+  $par = sybase_fetch_object($result);
+  $total_emails = number_format($par->emails);
 
- $qs = "select convert(char(8),count(*)) as emails from Email_Rank where PROJECT_ID=$project_id";
- $result = sybase_query($qs);
- sybase_data_seek($result,0);
- $par = sybase_fetch_object($result);
- $total_emails = number_format($par->emails);
- 
- $qs = "select convert(char(8),count(*)) as teams from Team_Rank where PROJECT_ID=$project_id";
- $result = sybase_query($qs);
- sybase_data_seek($result,0);
- $par = sybase_fetch_object($result);
- $total_teams = number_format($par->teams);
+  $qs = "select convert(char(8),count(*)) as teams from Team_Rank where PROJECT_ID=$project_id";
+  $result = sybase_query($qs);
+  sybase_data_seek($result,0);
+  $par = sybase_fetch_object($result);
+  $total_teams = number_format($par->teams);
 
- $tot_blocks_to_search = number_style_convert($proj_totalunits);
- $total_remaining = $proj_totalunits - $TOT_UNITS;
- $per_searched = number_format(100*($TOT_UNITS/$proj_totalunits),3);
- $bar_width = number_format(300*($TOT_UNITS/$proj_totalunits),0);
+  $tot_blocks_to_search = number_style_convert($proj_totalunits);
+  $total_remaining = $proj_totalunits - $TOT_UNITS;
+  $per_searched = number_format(100*($TOT_UNITS/$proj_totalunits),3);
+  $bar_width = number_format(300*($TOT_UNITS/$proj_totalunits),0);
 
-$constant_keys_in_one_block = 268435456;
- $tot_keys_searched = number_format(($TOT_UNITS*$constant_keys_in_one_block),0);
- $overall_rate = number_format((($TOT_UNITS*$constant_keys_in_one_block)/($time_working_raw*86400))/1000,0);
+  $constant_keys_in_one_block = 268435456;
+  $tot_keys_searched = number_format(($TOT_UNITS*$constant_keys_in_one_block),0);
+  $overall_rate = number_format((($TOT_UNITS*$constant_keys_in_one_block)/($time_working_raw*86400))/1000,0);
 
- sybase_query("set rowcount 1");
- $qs = "select convert(char(20),work_units) as work_units from Daily_Summary where PROJECT_ID=$project_id order by date desc";
- $result = sybase_query($qs);
- sybase_data_seek($result,0);
- $par = sybase_fetch_object($result);
- sybase_query("set rowcount 0");
- $yest_blocks = number_format($par->work_units);
- $yest_per =  number_format(100*($par->work_units/$proj_totalunits),3);
- $yest_rate = number_format((($par->work_units*$constant_keys_in_one_block)/86400)/1000,0);
+  sybase_query("set rowcount 1");
+  $qs = "select convert(char(20),work_units) as work_units from Daily_Summary where PROJECT_ID=$project_id order by date desc";
+  $result = sybase_query($qs);
+  sybase_data_seek($result,0);
+  $par = sybase_fetch_object($result);
+  sybase_query("set rowcount 0");
+  $yest_blocks = number_format($par->work_units);
+  $yest_per =  number_format(100*($par->work_units/$proj_totalunits),3);
+  $yest_rate = number_format((($par->work_units*$constant_keys_in_one_block)/86400)/1000,0);
 
 
- $odds = number_format($total_remaining / $par->work_units,0);
+  $odds = number_format($total_remaining / $par->work_units,0);
 ?>
   <center>
    <br>
--- static.sh
+++ static.sh
@@ -5,21 +5,22 @@ do_wget ( ) {
 	return
 }
 
-do_wget pc_index.php?project_id=24 cache/index_24.inc
-do_wget pc_index.php?project_id=25 cache/index_25.inc
-do_wget pc_index.php?project_id=5 cache/index_5.inc
-do_wget pc_index.php?project_id=8 cache/index_8.inc
+get_project ( ) {
+    do_wget pc_index.php?project_id=$1 cache/index_$1.inc
+    do_wget misc/pc_countries.php?project_id=$1\&source=o cache/countries_o_$1.inc
+    do_wget misc/pc_countries.php?project_id=$1\&source=y cache/countries_y_$1.inc
+}
 
-do_wget misc/pc_countries.php?project_id=24\&source=o cache/countries_o_24.inc
-do_wget misc/pc_countries.php?project_id=25\&source=o cache/countries_o_25.inc
-do_wget misc/pc_countries.php?project_id=5\&source=o cache/countries_o_5.inc 
-do_wget misc/pc_countries.php?project_id=8\&source=o cache/countries_o_8.inc 
-do_wget misc/pc_countries.php?project_id=24\&source=y cache/countries_y_24.inc
-do_wget misc/pc_countries.php?project_id=25\&source=y cache/countries_y_25.inc
-do_wget misc/pc_countries.php?project_id=5\&source=y cache/countries_y_5.inc
-do_wget misc/pc_countries.php?project_id=8\&source=y cache/countries_y_8.inc
+if [ x$1 = x ]; then
+    args="5 8 24 25"
+    echo "No projects specified, I assume you want all of them"
+else
+    args=$@
+fi
 
-#do_wget pc_index.php index.html
-#do_wget pc_countries.php countries.html
-#do_wget pc_cpu.php cpu.html
-#do_wget pc_money.php money.html
+echo "Updating static pages for projects $args"
+echo
+
+for project in $args; do
+    get_project $project
+done
