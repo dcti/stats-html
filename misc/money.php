a26239daf325752144c1638f99223497a5017275
decibel <decibel>
2002-12-17 04:51:43 +0000

    Formatting cleanup. Change to use new money_project format. Don't assume there's no money if the file doesn't exist.

--- misc/money.php
+++ misc/money.php
@@ -1,37 +1,33 @@
 <?php
 
-# $Id: money.php,v 1.3 2002/10/31 17:30:26 nugget Exp $
+# $Id: money.php,v 1.4 2002/12/17 04:51:43 decibel Exp $
+# vi: ts=2 sw=2 tw=120
 
- include "../etc/config.inc";
- include "../etc/modules.inc";
- include "../etc/project.inc";
+include "../etc/config.inc";
+include "../etc/modules.inc";
+include "../etc/project.inc";
 
- if($project_id == 205) {
-   $project_id = 5;
- }
+if($project_id == 205) {
+  $project_id = 5;
+}
 
- $filename = "../cache/money-$project_id.inc";
- $title = "Disposition of Prize Money";
+$title = "Disposition of Prize Money";
+$filename = "../cache/money_$project_id.inc";
 
- include "../templates/header.inc";
- print "   </tr>\n  </table>\n";
+include "../templates/header.inc";
 
- if(file_exists($filename)) {
-   include "$filename";
- } else {
-
-?>
-  <center>
-   <p>
-    <font  size="+2">
-     There is no prize money corresponding to this project.
-    </font>
-   </p>
+if(file_exists($filename)) {
+  include "$filename";
+} else {
+  display_last_update();
+  ?> 
+  <center>	
+    <p>
+      Apologies, these pages are currently being built. Please try again in a few minutes.
+    </p>
   </center>
-
-<?php
- }
-
- include "../templates/footer.inc";
+  <? 
+}
+include "../templates/footer.inc";
 
 ?>
