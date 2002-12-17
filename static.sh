1f1057ecc24a8c7880c70bc252c9d213a02cc8e5
decibel <decibel>
2002-12-17 05:01:06 +0000

    Don't regenerate money for old projects.

--- static.sh
+++ static.sh
@@ -9,7 +9,9 @@ get_project ( ) {
     do_wget pc_index.php?project_id=$1 cache/index_$1.inc
     do_wget misc/pc_countries.php?project_id=$1\&source=o cache/countries_o_$1.inc
     do_wget misc/pc_countries.php?project_id=$1\&source=y cache/countries_y_$1.inc
-    do_wget misc/pc_money.php?project_id=$1\&source=y cache/money_$1.inc
+    if [ $1 != 3 -a $1 != 5 ]; then
+        do_wget misc/pc_money.php?project_id=$1\&source=y cache/money_$1.inc
+    fi
 }
 
 if [ x$1 = x ]; then
