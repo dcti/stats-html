04c21f3d92114c07fdc4fe9e00462ed436e500e9
decibel <decibel>
2002-12-17 04:49:27 +0000

    Minor formatting and code cleanup.

--- misc/countries.php
+++ misc/countries.php
@@ -1,21 +1,26 @@
 <?
+# $Id: countries.php,v 1.4 2002/12/17 04:49:27 decibel Exp $
+# vi: ts=2 sw=2 tw=120
 
- include "../etc/config.inc";
- include "../etc/modules.inc";
- include "../etc/project.inc";
+include "../etc/config.inc";
+include "../etc/modules.inc";
+include "../etc/project.inc";
 
+$title = "Participating Countries";
+$filename = "../cache/countries_" . $source . "_" . $project_id . ".inc";
 
- $title = "Participating Countries";
+include "../templates/header.inc";
 
- include "../templates/header.inc";
- if( file_exists("../cache/countries_".$source."_$project_id.inc")) {
-	readfile( "../cache/countries_".$source."_$project_id.inc");
- } else {
- 	display_last_update();
+if( file_exists($filename)) {
+  readfile($filename);
+} else {
+  display_last_update();
   ?> 
-	<center>	
-	Apologies, these pages are currently being built. Please try again in a few minutes
-	</center>
+  <center>	
+    <p>
+      Apologies, these pages are currently being built. Please try again in a few minutes.
+    </p>
+  </center>
   <? 
 }
 ?>
