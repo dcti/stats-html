7c8b20c65205761ae3bf3d2c256d2eea1feded02
paul <paul>
2003-05-20 22:50:34 +0000

    initial pgsql stuff without yesterday stuff

--- pc_index.php
+++ pc_index.php
@@ -1,128 +1,106 @@
 <?
   # vi: ts=2 sw=2 tw=120 syntax=php
-  # $Id: pc_index.php,v 1.14 2003/05/20 19:45:34 paul Exp $
+  # $Id: pc_index.php,v 1.15 2003/05/20 22:50:34 paul Exp $
 
   $title = "Overall Project Stats";
 
   include "etc/config.inc";
   include "etc/modules.inc";
   include "etc/project.inc";
-
+	include "etc/projectstats.php";
+  
   ####
   # Daily summary
-  $qs = "select *
-          from Daily_summary
-          where PROJECT_ID=$project_id
-            and DATE = (select max(DATE) from Daily_summary where PROJECT_ID=$project_id)";
-  $result = sybase_query($qs);
-  sybase_data_seek($result,0);
-  $daysum = sybase_fetch_object($result);
-  $lastupdate = sybase_date_format_long($daysum->DATE);
+  $gprojstats = $gproj->get_current_stats();
+  //$lastupdate = sybase_date_format_long($gprojstats->get_stats_item('date'));
+  // sybase_deta_format?
+  $lastupdate = $gprojstats->get_stats_item('date');
   display_last_update('i');
 
-  $yest_scaled_work_units = number_format( (double) $daysum->WORK_UNITS * $proj_scale);
-  $yest_unscaled_work_units = number_format( (double) $daysum->WORK_UNITS);
-  $yest_emails = number_format($daysum->PARTICIPANTS);
-  $yest_teams = number_format($daysum->TEAMS);
-  $new_emails = number_format($daysum->PARTICIPANTS_NEW);
-  $new_teams = number_format($daysum->TEAMS_NEW);
+  $yest_scaled_work_units = number_format( (double) $gprojstats->get_stats_item('work_units') * $gproj->get_scale());
+  $yest_unscaled_work_units = number_format( (double) $gprojstats->get_stats_item('work_units'));
+  $yest_emails = number_format($gprojstats->get_stats_item('participants'));
+  $yest_teams = number_format($gprojstats->get_stats_item('teams'));
+  $new_emails = number_format($gprojstats->get_stats_item('participants_new'));
+  $new_teams = number_format($gprojstats->get_stats_item('teams_new'));
 
   ####
   # Total work, time working
-  sybase_query("set rowcount 0");
-  $qs = "select sum(WORK_UNITS) as TOT_UNITS, datediff(dd,min(date),max(date))+1 as TIME_WORKING
-          from Daily_Summary
-          where PROJECT_ID=$project_id";
-  $result = sybase_query($qs);
-  sybase_data_seek($result,0);
-  $par = sybase_fetch_object($result);
 
-  $time_working_raw = $par->TIME_WORKING;
-  $time_working = number_format($par->TIME_WORKING);
-  $TOT_UNITS = $par->TOT_UNITS;
-  $tot_unscaled_work_units = number_format( (double) $TOT_UNITS);
-  $tot_scaled_work_units = number_format( (double) $TOT_UNITS * $proj_scale);
+  $time_working_raw = $gprojstats->get_time_working();
+  $time_working = number_format($gprojstats->get_time_working());
+  $tot_unscaled_work_units = number_format( (double) $gprojstats->get_tot_units());
+  $tot_scaled_work_units = number_format( (double) $gprojstats->get_tot_units() * $gproj->get_scale());
 
-  ####
-  # Total Emails
-  $qs = "select convert(char(8),count(*)) as emails from Email_Rank where PROJECT_ID=$project_id";
-  $result = sybase_query($qs);
-  sybase_data_seek($result,0);
-  $par = sybase_fetch_object($result);
-  $total_emails = number_format($par->emails);
-
-  ####
-  # Total Teams
-  $qs = "select convert(char(8),count(*)) as teams from Team_Rank where PROJECT_ID=$project_id";
-  $result = sybase_query($qs);
-  sybase_data_seek($result,0);
-  $par = sybase_fetch_object($result);
-  $total_teams = number_format($par->teams);
+  $total_emails = number_format($gprojstats->get_total_emails());
+  $total_teams = number_format($gprojstats->get_total_teams());
 
   ####
   # Percent complete
   $tot_unscaled_units_to_search = number_style_convert($gproj->get_total_units());
-  $tot_scaled_units_to_search = number_style_convert($gproj->get_total_units() * $proj_scale);
-  $total_remaining = $gproj->get_total_units() - $TOT_UNITS;
-  $per_searched = number_format(100*($TOT_UNITS/$gproj->get_total_units()),3);
-  $bar_width = number_format(300*($TOT_UNITS/$gproj->get_total_units()),0);
+  $tot_scaled_units_to_search = number_style_convert($gproj->get_total_units() * $gproj->get_scale());
+  $total_remaining = $gproj->get_total_units() - $gprojstats->get_tot_units();
+  $per_searched = number_format(100*($gprojstats->get_tot_units()/$gproj->get_total_units()),3);
+  $bar_width = number_format(300*($gprojstats->get_tot_units()/$gproj->get_total_units()),0);
 
   ####
   # Overall Rate
-  $overall_unscaled_rate = number_format(( ($TOT_UNITS) / ($time_working_raw*86400) ),0);
-  $overall_scaled_rate = number_format(( ($TOT_UNITS * $proj_scale) / ($time_working_raw*86400) ),0);
+  $overall_unscaled_rate = number_format(( ($gprojstats->get_tot_units()) / ($time_working_raw*86400) ),0);
+  $overall_scaled_rate = number_format(( ($gprojstats->get_tot_units() * $gproj->get_scale()) / ($time_working_raw*86400) ),0);
 
   ####
   # Yesterday Rate
-  sybase_query("set rowcount 1");
+  /*sybase_query("set rowcount 1");
   $qs = "select convert(char(20),work_units) as work_units from Daily_Summary where PROJECT_ID=$project_id order by date desc";
   $result = sybase_query($qs);
   sybase_data_seek($result,0);
   $par = sybase_fetch_object($result);
   sybase_query("set rowcount 0");
+  
   $yest_unscaled_work_units = number_format($par->work_units);
-  $yest_scaled_work_units = number_format($par->work_units * $proj_scale);
+  $yest_scaled_work_units = number_format($par->work_units * $gproj->get_scale());
   $yest_per =  number_format(100*($par->work_units / $gproj->get_total_units()),6);
   $yest_unscaled_rate = number_format(( ($par->work_units) / (86400) ),0);
-  $yest_scaled_rate = number_format(( ($par->work_units * $proj_scale) / (86400) ),0);
+  $yest_scaled_rate = number_format(( ($par->work_units * $gproj->get_scale()) / (86400) ),0);
 
 
   $odds = number_format($total_remaining / $par->work_units,0);
+  */
 ?>
    <br>
    <p>
      Aggregate Statistics
    </p>
    <table>
-<? if ($proj_totalunits > 0 ) { ?>
+<? if ($gproj->get_total_units() > 0 ) { ?>
     <tr>
-     <td>Total <?=$gproj->get_scaled_unit_name?> to Search:</td>
+     <td>Total <?=$gproj->get_scaled_unit_name()?> to Search:</td>
      <td align="right"><?=$tot_scaled_units_to_search?></td>
     </tr>
 <? } ?>
     <tr>
-     <td>Total <?=$gproj->get_scaled_unit_name?> Tested:</td>
+     <td>Total <?=$gproj->get_scaled_unit_name()?> Tested:</td>
      <td align="right"><?=$tot_scaled_work_units?></td>
     </tr>
     <tr>
      <td>Overall Rate:</td>
-     <td align="right"><?=$overall_scaled_rate?> <?=$gproj->get_scaled_unit_name?>/sec</td>
+     <td align="right"><?=$overall_scaled_rate?> <?=$gproj->get_scaled_unit_name()?>/sec</td>
     </tr>
-<? if ($proj_totalunits > 0 ) { ?>
+<? if ($gproj->get_total_units() > 0 ) { ?>
     <tr>
-     <td>Total <?=$proj_unscaled_unit_name?> to Search:</td>
+     <td>Total <?=$gproj->get_unscaled_unit_name?> to Search:</td>
      <td align="right"><?=$tot_unscaled_units_to_search?></td>
     </tr>
 <? } ?>
     <tr>
-     <td>Total <?=$proj_unscaled_unit_name?> Tested:</td>
+     <td>Total <?=$gproj->get_unscaled_unit_name()?> Tested:</td>
      <td align="right"><?=$tot_unscaled_work_units?></td>
     </tr>
     <tr>
      <td>Overall Rate:</td>
-     <td align="right"><?=$overall_unscaled_rate?> <?=$proj_unscaled_unit_name?>/sec</td>
+     <td align="right"><?=$overall_unscaled_rate?> <?=$gproj->get_unscaled_unit_name()?>/sec</td>
     </tr>
-<? if ($proj_totalunits > 0 ) { ?>
+<? if ($gproj->get_total_units() > 0 ) { ?>
      <tr>
      <td>Percent Complete:</td>
      <td align="right"><?=$per_searched?>%</td>
@@ -150,18 +128,18 @@
      Current Information
   </p>
   <p>
-      <?=$yest_scaled_work_units?> <?=$gproj->get_scaled_unit_name?> were completed yesterday
+      <?=$yest_scaled_work_units?> <?=$gproj->get_scaled_unit_name()?> were completed yesterday
         <? if ($gproj->get_total_units() > 0 ) { ?>
         (<?=$yest_per?>% of the keyspace)<br>
         <? } ?>
-       at a sustained rate of <?=$yest_scaled_rate?> <?=$gproj->get_scaled_unit_name?>/sec.
+       at a sustained rate of <?=$yest_scaled_rate?> <?=$gproj->get_scaled_unit_name()?>/sec.
   </p>
   <p>
-      <?=$yest_unscaled_work_units?> <?=$proj_unscaled_unit_name?> were completed yesterday
+      <?=$yest_unscaled_work_units?> <?=$gproj->get_unscaled_unit_name()?> were completed yesterday
         <? if ($gproj->get_total_units() > 0 ) { ?>
         (<?=$yest_per?>% of the keyspace)<br>
         <? } ?>
-       at a sustained rate of <?=$yest_unscaled_rate?> <?=$proj_unscaled_unit_name?>/sec.
+       at a sustained rate of <?=$yest_unscaled_rate?> <?=$gproj->get_unscaled_unit_name()?>/sec.
   </p>
   <? if ($gproj->get_total_units() > 0 ) { ?>
   <p>
