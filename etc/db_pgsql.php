317fe4d6b591c9c593d6bc03ada7ffce2ecd495f
decibel <decibel>
2003-07-10 19:31:11 +0000

    Add ogr_graphing code

---
+++ etc/db_pgsql.php
@@ -0,0 +1,211 @@
+<?php
+/**
+ * Handles the DB abstraction layer for pgsql
+ * 
+ * @version 1.0
+ */
+class DB {
+    /**
+     * Database name to connect to.
+     * 
+     * @var string 
+     */
+    var $conn_string;
+
+    /**
+     * Database Server to connect to.
+     * 
+     * @var string 
+     */
+    var $server;
+
+    var $link_id = 0;
+    var $query_id = 0;
+    var $record = array();
+
+    var $errdesc = "";
+    var $reporterror = 1;
+	var $_connected = false;
+
+    /**
+     * Constructor
+     */
+    function DB($conn_string)
+    {
+        $this -> conn_string = $conn_string;
+        $this -> _connect();
+    } 
+
+    /**
+     * Connect to the database
+     * 
+     * @access private 
+     */
+    function _connect()
+    {
+        if(0 == $this -> link_id) {
+            $this -> link_id = pg_pconnect($this -> conn_string);
+            if(pg_connection_status() == PGSQL_CONNECTION_BAD) {
+                $this -> halt("pgsql connection failed");
+            } 
+			$this->_connected = true;
+        } 
+    } 
+
+    /**
+     * Return last sybase error message.
+     * 
+     * @return string returns the last message reported by the server
+     * @access public 
+     */
+    function get_last_error()
+    {
+        return pg_last_error($this -> link_id);
+    } 
+
+    /**
+     * Run Query
+     * 
+     * @param string $query_string Databasename to use
+     * @return int Query id
+     * @access public 
+     * @todo -c"DB" Implement DB.query failed query error handling
+     */
+    function query($query_string)
+    {
+        $this -> query_id = pg_query (/*$this -> link_id,*/ $query_string);
+        if(!$this -> query_id) {
+           // $this -> halt("Invalid SQL: " . $query_string);
+        } 
+        return $this -> query_id;
+    } 
+
+    // Function ....: fetch_array
+    // Description .: Will take a query and fetch it into an associative array
+
+    function fetch_array($query_id = -1, $query_string = "")
+    { 
+        // retrieve row
+        if($query_id != -1)
+            $this -> query_id = $query_id;
+
+        if(isset($this -> query_id)) {
+            $this -> record = pg_fetch_array($this -> query_id);
+        } else {
+            if(!empty($query_string)) {
+                $this -> halt("Invalid query id (" . $this -> query_id . ") on this query: $query_string");
+            } else {
+                $this -> halt("Invalid query id " . $this -> query_id . " specified");
+            } 
+        } // end if
+        return $this -> record;
+    } 
+    // Function ....: fetch_object
+    function fetch_object($query_id = -1, $query_string = "")
+    { 
+        // retrieve row
+        if($query_id != -1)
+            $this -> query_id = $query_id;
+
+        if(isset($this -> query_id)) {
+            $this -> record = pg_fetch_object($this -> query_id);
+        } else {
+            if(!empty($query_string)) {
+                $this -> halt("Invalid query id (" . $this -> query_id . ") on this query: $query_string");
+            } else {
+                $this -> halt("Invalid query id " . $this -> query_id . " specified");
+            } 
+        } // end if
+        return $this -> record;
+    } 
+
+    // //////////////////////
+    // Function: fetch_paged_result
+    function fetch_paged_result($query_id, $start = -1, $limit = -1)
+    {
+      if($start > 1)
+      {
+        $this->data_seek($start-1, $query_id);
+      }
+      $ctr = 0;
+      while($tmp = $this->fetch_object($query_id))
+      {
+        $result[] = $tmp;
+        $ctr++;
+        if($limit != -1 && $ctr >= $limit) break;
+      }
+      return $result;
+    }
+
+    // //////////////////////
+    // Function ....: free result
+    // Description .: returns the memory (although PHP should do this automagically)
+    // //////////////////////
+    function free_result($query_id = -1)
+    { 
+        // retrieve row
+        if($query_id != -1) {
+            $this -> query_id = $query_id;
+        } 
+        return @pg_free_result($this -> query_id);
+    } // end function free_result   
+    // //////////////////////
+    // Function ....: query_first
+    // Description .: Executes a query and returns an object.
+    // Useful if you are expecting a single row to be returned so you don't have to loop
+    // //////////////////////
+    function query_first($query_string)
+    { 
+        // does a query and returns first row
+        $query_id = $this -> query($query_string);
+        $returnobj = $this -> fetch_object($query_id, $query_string);
+        $this -> free_result($query_id);
+        return $returnobj;
+    } // end function query_first   
+    // //////////////////////
+    // Function ....: data_seek
+    // Description .: If you want to move around, generally don't use
+    // //////////////////////
+    function data_seek($pos = 1, $query_id = -1)
+    { 
+        // goes to row $pos
+        if($query_id != -1) {
+            $this -> query_id = $query_id;
+        } 
+        return pg_result_seek ($this -> query_id, $pos);
+    } 
+
+    // Function ....: num_rows
+    // Description .: if you want the num rows returned
+    function num_rows($query_id = -1)
+    { 
+        // returns number of rows in query
+        if($query_id != -1) {
+            $this -> query_id = $query_id;
+        } 
+        return pg_num_rows($this -> query_id);
+    }
+	
+	// Function ....: close
+    // Description .: closes (although PHP should do this automagically)
+    function close()
+    {
+        return pgsql_close($this -> link_id);
+    } 
+
+    // Function ....: halt
+    // Description .: If the DB class encounters an error, pretty it up first and don't show them the SQL stuff
+    // and then mail a message saying what happened to you
+    function halt($msg)
+    {
+        $this -> errdesc = $this->get_last_error();
+
+        // prints warning message when there is an error
+        if($this -> reporterror == 1) {
+
+            exit;
+        } 
+    } 
+} 
+
+?>
---
+++ project/ogr_graph.php
@@ -0,0 +1,119 @@
+<?php
+// ************ OGR Status Graphing Module           ***
+// ************ Filename: ogr_graph.pl               ***
+// ************ Author: TheJet, May 2003            ***
+// ************ Based on code by: Dyyryath, Jun 2001 ***
+// ************                                      ***
+// ************ Version: 1.0a                        ***
+
+// ************ Setup any USE statements and initialize variables
+include "../etc/config.inc";
+include "../etc/db_pgsql.php";
+include "../etc/jpgraph/jpgraph.php";
+include "../etc/jpgraph/jpgraph_line.php";
+
+$history                 = "60";
+$dbDSN                   = "dbname=ogr";
+$table                   = "ogr_complete";
+$numeric_scale           = 1000000;
+$scale_text              = "millions";
+
+
+// ************ Setup the CGI data
+$project = (int)$_GET["project_id"];
+
+// ************ Connect to the database
+$db = new DB($dbDSN);
+if($db == 0)
+{
+  print "Error connecting to database\n";
+  print "Last Error: " + $db->get_last_error();
+  exit();
+}
+
+// ************ Get the data
+$queryData = $db->query("SELECT rundate, count, pass1, pass2 FROM $table WHERE project_id = $project ORDER BY rundate DESC LIMIT $history");
+
+if($queryData == 0)
+{
+  print "Error performing query";
+  exit();
+}
+
+while($result = $db->fetch_array($queryData))
+{ 
+    $xData1[] = (int)$result{'count'}/$numeric_scale;
+    $xData2[] = (int)$result{'pass1'}/$numeric_scale;
+    $xData3[] = (int)$result{'pass2'}/$numeric_scale;
+    $xData4[] = ($result{'pass1'}/$result{'count'} + $result{'pass2'}/$result{'count'}) /2 * 100;
+    $xLabel[] = $result{'rundate'};
+    
+    if ($result{'count'} > $max) { $max = (int)$result{'count'}; }
+}
+
+// ************ Change the graph orientation
+$xData1 = array_reverse($xData1);
+$xData2 = array_reverse($xData2);
+$xData3 = array_reverse($xData3);
+$xData4 = array_reverse($xData4);
+$xLabel = array_reverse($xLabel);
+
+$graph = new Graph(450,300,"auto");
+$graph->SetScale("textlin", 0, (int)$max/$numeric_scale);
+$graph->SetY2Scale("lin", 0, 100);
+$graph->title->Set("OGR-$project Completion Statistics");
+$graph->yaxis->SetTitle("Stubs ($scale_text)", "middle");
+$graph->yaxis->SetTitleMargin(30);
+$graph->yaxis->title->SetFont(FF_FONT1,FS_BOLD);
+$graph->y2axis->SetTitle("% complete (aggregate)", "middle");
+$graph->y2axis->title->SetFont(FF_FONT1,FS_BOLD);
+$graph->y2axis->SetTitleMargin(35);
+
+// Use built in font
+$graph->title->SetFont(FF_FONT2,FS_BOLD);
+
+// Make the margin around the plot a little bit bigger
+// then default
+$graph->img->SetMargin(60,60,40,70);
+#$graph->img->SetAntiAliasing();
+
+// Slightly adjust the legend from it's default position in the
+// top right corner to middle right side
+$graph->legend->Pos(0.5,0.90,"center","center");
+$graph->legend->SetLayout(LEGEND_HOR);
+
+// Set the x-axis labels
+$graph->xaxis->SetTickLabels($xLabel);
+#$graph->xaxis->SetTextLabelInterval(5);
+$graph->xaxis->SetTextTickInterval(12);
+
+// Create a red line plot
+//$p1 = new LinePlot($xData1);
+//$p1->SetColor("red");
+//$p1->SetLegend("Stub Count");
+//$graph->Add($p1);
+
+// Create a red line plot
+$p2 = new LinePlot($xData2);
+$p2->SetColor("blue");
+$p2->SetLegend("Pass1 Stubs Completed");
+#$p2->SetWeight(1);
+$graph->Add($p2);
+
+// Create a red line plot
+$p3 = new LinePlot($xData3);
+$p3->SetColor("red");
+$p3->SetLegend("Pass2 Stubs Completed");
+#$p3->SetWeight(3);
+$graph->Add($p3);
+
+// Add the %-complete graph
+$p4 = new LinePlot($xData4);
+$p4->SetColor("#770077");
+$p4->SetLegend("%-complete");
+#$p4->SetWeight(1.25);
+$graph->AddY2($p4);
+
+// Finally output the  image
+$graph->Stroke();
+?>
