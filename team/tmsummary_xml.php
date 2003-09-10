591cc0af330ceb71efc9aab24a61a35b77aececf
thejet <thejet>
2003-09-10 02:47:11 +0000

    remove easily calculated fields from export, remove numeric formatting

--- team/tmsummary_xml.php
+++ team/tmsummary_xml.php
@@ -1,6 +1,6 @@
 <?
 // vi: ts=2 sw=2 tw=120
-// $Id: tmsummary_xml.php,v 1.2 2003/09/05 20:38:27 thejet Exp $
+// $Id: tmsummary_xml.php,v 1.3 2003/09/10 02:47:11 thejet Exp $
 
 // Variables Passed in url:
 //  team == team id to display
@@ -29,16 +29,6 @@ if ($team->get_id() == 0) {
 
 $neighbors = $team->get_neighbors();
 $numneighbors = count($neighbors);
-
-/*
-$qs = "select *
-        from Daily_Summary nolock
-        where PROJECT_ID = $project_id
-          and DATE = (select max(DATE) from Daily_Summary where project_id=$project_id)";
-sybase_query("set rowcount 0");
-$result = sybase_query($qs);
-$yest_totals = sybase_fetch_object($result);
-*/
 ?>
 <team-summary id="<?= $tm ?>" project="<?= $gproj->get_name() ?>" project-id="<?= $gproj->get_id() ?>" date="<?= $lastupdate ?>">
   <name><?= safe_display($team->get_name()) ?></name>
@@ -48,27 +38,21 @@ $yest_totals = sybase_fetch_object($result);
   <membership><?if ($team->get_show_members()=="NO") {?>private<?} else { if ($team->get_show_members()=="PAS") {?>password<?} else {?>public<?} }?></membership>
   <stats date="<?= safe_display($stats->get_stats_item("last_date")) ?>">
     <stat name="overall-rank" unit="" value="<?= $stats->get_stats_item("overall_rank") ?>" change="<?= $stats->get_stats_item('overall_rank_previous') - $stats->get_stats_item("overall_rank") ?>"/>
-    <stat name="overall-work" unit="<?= $gproj->get_scaled_unit_name() ?>" value="<?= number_style_convert($stats->get_stats_item("work_total") * $gproj->get_scale()) ?>"/>
-    <stat name="overall-work-persec" unit="<?= $gproj->get_scaled_unit_name() ?>/sec" value="<?= number_style_convert($stats->get_stats_item("work_total") * $gproj->get_scale() / (86400 * $stats->get_stats_item("days_working")), 3) ?>"/>
-    <stat name="overall-work-permember" unit="<?= $gproj->get_scaled_unit_name() ?>/member" value="<?= number_style_convert($stats->get_stats_item("work_total") * $gproj->get_scale() / $stats->get_stats_item("members_overall")) ?>"/>
-    <!-- Note, you can insert unscaled information here, if you're interested -->
+    <stat name="overall-work" unit="<?= $gproj->get_scaled_unit_name() ?>" value="<?= round($stats->get_stats_item("work_total") * $gproj->get_scale(),0) ?>"/>
 
     <? if ($stats->get_stats_item("work_today") > 0) { ?>
     <stat name="day-rank" unit="" value="<?= $stats->get_stats_item("day_rank") ?>" change="<?= $stats->get_stats_item('day_rank_previous') - $stats->get_stats_item("day_rank") ?>"/>
-    <stat name="day-work" unit="<?= $gproj->get_scaled_unit_name() ?>" value="<?= number_style_convert($stats->get_stats_item("work_today") * $gproj->get_scale()) ?>"/>
-    <stat name="day-work-persec" unit="<?= $gproj->get_scaled_unit_name() ?>" value="<?= number_style_convert($stats->get_stats_item("work_today") * $gproj->get_scale() / 86400, 3) ?>"/>
-    <stat name="day-work-permember" unit="<?= $gproj->get_scaled_unit_name() ?>" value="<?= number_style_convert($stats->get_stats_item("work_today") * $gproj->get_scale() / $stats->get_stats_item("members_today")) ?>"/>
-    <!-- Note, you can insert unscaled information here, if you're interested -->
+    <stat name="day-work" unit="<?= $gproj->get_scaled_unit_name() ?>" value="<?= round($stats->get_stats_item("work_today") * $gproj->get_scale(),0) ?>"/>
     <? } ?>
 
-    <stat name="time-working" unit="days" value="<?= number_style_convert($stats->get_stats_item("days_working")) ?>"/>
+    <stat name="time-working" unit="days" value="<?= round($stats->get_stats_item("days_working"),0) ?>"/>
     <? if ($gproj->get_total_units() > 0) { ?>
-    <stat name="odds" unit="" value="<?= number_style_convert($yest_totals->WORK_UNITS / $par->WORK_TODAY) ?>"/>
+    <stat name="odds" unit="" value="<?= round($yest_totals->WORK_UNITS / $par->WORK_TODAY,0) ?>"/>
     <? } ?>
 
-    <stat name="members-overall" unit="" value="<?=number_style_convert($stats->get_stats_item("members_overall"))?>"/>
-    <stat name="members-current" unit="" value="<?=number_style_convert($stats->get_stats_item("members_current"))?>"/>
-    <stat name="members-day" unit="" value="<?=number_style_convert($stats->get_stats_item("members_today"))?>"/>
+    <stat name="members-overall" unit="" value="<?=round($stats->get_stats_item("members_overall"),0)?>"/>
+    <stat name="members-current" unit="" value="<?=round($stats->get_stats_item("members_current"),0)?>"/>
+    <stat name="members-day" unit="" value="<?=round($stats->get_stats_item("members_today"),0)?>"/>
   </stats>
   <nearby-teams>
     <?
@@ -81,27 +65,21 @@ $yest_totals = sybase_fetch_object($result);
       <membership><?if ($tmpTeam->get_show_members()=="NO") {?>private<?} else { if ($tmpTeam->get_show_members()=="PAS") {?>password<?} else {?>public<?} }?></membership>
       <stats date="<?= safe_display($tmpStats->get_stats_item("last_date")) ?>">
         <stat name="overall-rank" unit="" value="<?=$tmpStats->get_stats_item("overall_rank") ?>" change="<?= $tmpStats->get_stats_item('overall_rank_previous') - $tmpStats->get_stats_item("overall_rank") ?>"/>
-        <stat name="overall-work" unit="<?= $gproj->get_scaled_unit_name() ?>" value="<?= number_style_convert($tmpStats->get_stats_item("work_total") * $gproj->get_scale()) ?>"/>
-        <stat name="overall-work-persec" unit="<?= $gproj->get_scaled_unit_name() ?>/sec" value="<?= number_style_convert($tmpStats->get_stats_item("work_total") * $gproj->get_scale() / (86400 * $tmpStats->get_stats_item("days_working")), 3) ?>"/>
-        <stat name="overall-work-permember" unit="<?= $gproj->get_scaled_unit_name() ?>/member" value="<?= number_style_convert($tmpStats->get_stats_item("work_total") * $gproj->get_scale() / $tmpStats->get_stats_item("members_overall")) ?>"/>
-        <!-- Note, you can insert unscaled information here, if you're interested -->
+        <stat name="overall-work" unit="<?= $gproj->get_scaled_unit_name() ?>" value="<?= round($tmpStats->get_stats_item("work_total") * $gproj->get_scale(),0) ?>"/>
 
         <? if ($tmpStats->get_stats_item("work_today") > 0) { ?>
         <stat name="day-rank" unit="" value="<?= $tmpStats->get_stats_item("day_rank") ?>" change="<?= $tmpStats->get_stats_item('day_rank_previous') - $tmpStats->get_stats_item("day_rank") ?>"/>
-        <stat name="day-work" unit="<?= $gproj->get_scaled_unit_name() ?>" value="<?= number_style_convert($tmpStats->get_stats_item("work_today") * $gproj->get_scale()) ?>"/>
-        <stat name="day-work-persec" unit="<?= $gproj->get_scaled_unit_name() ?>" value="<?= number_style_convert($tmpStats->get_stats_item("work_today") * $gproj->get_scale() / 86400, 3) ?>"/>
-        <stat name="day-work-permember" unit="<?= $gproj->get_scaled_unit_name() ?>" value="<?= number_style_convert($tmpStats->get_stats_item("work_today") * $gproj->get_scale() / $tmpStats->get_stats_item("members_today")) ?>"/>
-        <!-- Note, you can insert unscaled information here, if you're interested -->
+        <stat name="day-work" unit="<?= $gproj->get_scaled_unit_name() ?>" value="<?= round($tmpStats->get_stats_item("work_today") * $gproj->get_scale(),0) ?>"/>
         <? } ?>
 
-        <stat name="time-working" unit="days" value="<?= number_style_convert($tmpStats->get_stats_item("days_working")) ?>"/>
+        <stat name="time-working" unit="days" value="<?= round($tmpStats->get_stats_item("days_working"),0) ?>"/>
         <? if ($gproj->get_total_units() > 0) { ?>
-        <stat name="odds" unit="" value="<?= number_style_convert($yest_totals->WORK_UNITS / $teamrec->WORK_TODAY) ?>"/>
+        <stat name="odds" unit="" value="<?= round($yest_totals->WORK_UNITS / $teamrec->WORK_TODAY,0) ?>"/>
         <? } ?>
 
-        <stat name="members-overall" unit="" value="<?=number_style_convert($tmpStats->get_stats_item("members_overall"))?>"/>
-        <stat name="members-current" unit="" value="<?=number_style_convert($tmpStats->get_stats_item("members_current"))?>"/>
-        <stat name="members-day" unit="" value="<?=number_style_convert($tmpStats->get_stats_item("members_today"))?>"/>
+        <stat name="members-overall" unit="" value="<?=round($tmpStats->get_stats_item("members_overall"),0)?>"/>
+        <stat name="members-current" unit="" value="<?=round($tmpStats->get_stats_item("members_current"),0)?>"/>
+        <stat name="members-day" unit="" value="<?=round($tmpStats->get_stats_item("members_today"),0)?>"/>
       </stats>
     </team-summary>
     <?
