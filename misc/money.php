f9322daed84226ecd2ef47f3ef13cae4fe8a211f
paul <paul>
2004-04-22 20:18:34 +0000

    remove project id

--- misc/money.php
+++ misc/money.php
@@ -1,15 +1,11 @@
 <?php
 
-# $Id: money.php,v 1.4 2002/12/17 04:51:43 decibel Exp $
+# $Id: money.php,v 1.5 2004/04/22 20:18:34 paul Exp $
 # vi: ts=2 sw=2 tw=120
 
 include "../etc/config.inc";
 include "../etc/modules.inc";
 include "../etc/project.inc";
-
-if($project_id == 205) {
-  $project_id = 5;
-}
 
 $title = "Disposition of Prize Money";
 $filename = "../cache/money_$project_id.inc";
@@ -20,13 +16,13 @@ if(file_exists($filename)) {
   include "$filename";
 } else {
   display_last_update();
-  ?> 
-  <center>	
+  ?>
+  <center>
     <p>
       Apologies, these pages are currently being built. Please try again in a few minutes.
     </p>
   </center>
-  <? 
+  <?
 }
 include "../templates/footer.inc";
 
