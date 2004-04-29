924750eb16f5301607ef919bb17b3d9baff71dc3
paul <paul>
2004-04-29 20:37:34 +0000

    updates

--- etc/project.php
+++ etc/project.php
@@ -1,53 +1,44 @@
 <?php
-// $Id: project.php,v 1.7 2003/10/23 02:30:24 thejet Exp $
-// ==========================================
-// file: project.inc
-// This file contains the classes which
-// represent a project in the stats
-// system.  It abstracts the concept of a
-// project and its stats into objects.
-// ==========================================
+// $Id: project.php,v 1.8 2004/04/29 20:37:34 paul Exp $
+
 /**
  * This class represents a project
- * 
+ *
  * This class represents a project in the stats system.
  * Although PHP supports it, member variables should _not_
  * be accessed directly, please adhere to the published
  * public interface, as private methods and signatures
  * can change at any time.
- * 
- * @access public 
+ *
+ * @access public
  */
 class Project {
     /**
-     * ** Internal Class variables go here **
+     * database object
      */
-    // This needs to be filled in
     var $_db;
-    /**
-     * ** END Internal Class variables **
-     */
+
 
     /**
      * The Id for the current project (read only)
-     * 
-     * @access public 
+     *
+     * @access public
      * @type int
      */
     var $_id;
 
     /**
      * The type of the current project
-     * 
-     * @access public 
+     *
+     * @access public
      * @type string
      */
     var $type;
 
     /**
      * The name of the current project
-     * 
-     * @access public 
+     *
+     * @access public
      * @type string
      */
 
@@ -57,11 +48,12 @@ class Project {
     var $_scale;
     var $_scaled_unit_name;
     var $_unscaled_unit_name;
+    var $_stats;
 
     /**
      * The status of the current project
-     * 
-     * @access public 
+     *
+     * @access public
      * @type string
      */
     var $Status;
@@ -81,37 +73,37 @@ class Project {
     function get_total_units()
     {
         return $this -> _totalunits;
-    } 
+    }
     function get_prize()
     {
         return $this -> _state -> prize;
-    } 
+    }
 
     function get_scaled_unit_name()
     {
         return $this -> _scaled_unit_name;
-    } 
+    }
 
     function get_unscaled_unit_name()
     {
         return $this -> _unscaled_unit_name;
-    } 
+    }
 
     function get_scale()
     {
         return $this -> _scale;
-    } 
+    }
 
     function get_type()
     {
         return $this -> _type;
-    } 
+    }
 
     /**
      * Instantiates a new project object, and loads it with the specified project's information.
-     * 
-     * @access public 
-     * @return void 
+     *
+     * @access public
+     * @return void
      * @param DBClass $ The database connectivity to use
      *             int The ID of the project to load
      */
@@ -121,20 +113,21 @@ class Project {
         $this -> _id = $id;
         if($id > 0)
             $this -> load($id);
-    } 
+    }
 
     /**
      * Loads the requested participant object using the current database connection
-     * 
-     * @access public 
-     * @return bool 
+     *
+     * @access public
+     * @return bool
      * @param int $ The ID of the project to load
      */
     function load($id)
     {
-        $qs = "	select * from Projects where PROJECT_ID = $id";
+        $qs = "SELECT * FROM projects WHERE project_id = " . $this->_db->prepare_int($id);
         $prj_info = $this -> _db -> query_first($qs);
-        if($prj_info == FALSE) return;
+        if($prj_info == FALSE)
+            return;
 
         $this -> _name = $prj_info -> name;
         $this -> _type = $prj_info -> project_type;
@@ -143,61 +136,60 @@ class Project {
         $this -> _scaled_unit_name = $prj_info -> scaled_work_unit_name;
         $this -> _unscaled_unit_name = $prj_info -> unscaled_work_unit_name;
         $this -> _state = $prj_info ;
-    } 
+    }
 
     /**
      * Returns the current ProjectStats object for this project
-     * 
+     *
      * This routine is "load-on-demand", meaning that the data is retrieved from the DB
      * on first access, and then from a local variable thereafter.
-     * 
-     * @access public 
-     * @return ProjectStats 
+     *
+     * @access public
+     * @return ProjectStats
      */
     function getCurrentStats()
     {
-    } 
+    }
 
     /**
      * Returns the requested amount of historical stats information for this project
-     * 
+     *
      * This routine retrieves the requested number of previous days of stats information
      * for this project.  You specify the start date, and the number of previous days
      * to retrieve.
-     * 
-     * @access public 
+     *
+     * @access public
      * @return ProjectStats []
      * @param date $ The date to start retrieval
      *             int The number of days prior to $start to retrieve data for
      */
     function getStatsHistory($start, $getDays)
     {
-    } 
+    }
 
     /**
      * Turns the current database-oriented object/array into an internal representation
-     * 
+     *
      * This routine provides for an easy way to turn database-oriented objects/arrays
      * into the generic internal representation that we're using, avoiding a database hit
      * in cases where you already have the project's information.
      * This is functionally similar to object deserialization.
-     * 
-     * @access protected 
-     * @return bool 
+     *
+     * @access protected
+     * @return bool
      * @param DBVariant $ This is the object/array from the database server which contains the data for the desired project
      */
     function explode($prjInfo)
     {
-    } 
+    }
 
-    var $_stats;
     function get_current_stats()
     {
         if($this -> _stats == null) {
             $this -> _stats = new ProjectStats($this -> _db, $this -> _id);
-        } 
+        }
         return $this -> _stats;
-    } 
-} 
+    }
+}
 
 ?>
