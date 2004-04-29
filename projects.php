22ce699505879cb867c14934ff613d71eb632ffe
paul <paul>
2004-04-29 20:05:38 +0000

    formatting

--- projects.php
+++ projects.php
@@ -1,18 +1,18 @@
 <?
 
- include "etc/config.inc";
- include "etc/modules.inc";
- include "etc/project.inc";
+include "etc/config.inc";
+include "etc/modules.inc";
+include "etc/project.inc";
 
+$title = "Overall Project Stats";
 
- $title = "Overall Project Stats";
-
- include "templates/header.inc";
- if( file_exists("cache/index_$project_id.inc")) {
-	readfile( "cache/index_$project_id.inc");
- } else {
-  display_last_update();
-  trigger_error("Apologies, these pages are currently being built. Please try again in a few minutes",E_USER_ERROR);
+include "templates/header.inc";
+if( file_exists("cache/index_$project_id.inc")) {
+    readfile( "cache/index_$project_id.inc");
+} else {
+    display_last_update();
+    trigger_error("Apologies, these pages are currently being built. Please try again in a few minutes",E_USER_WARNING);
 }
+
+include "templates/footer.inc";
 ?>
-<?include "templates/footer.inc";?>
