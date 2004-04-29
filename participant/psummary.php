42d510c63b3994ff9ead26f846c5a5600f48bba8
paul <paul>
2004-04-29 23:17:10 +0000

    formatting + other updates, add link to phistory graph

--- participant/psummary.php
+++ participant/psummary.php
@@ -1,14 +1,15 @@
 <?
 // vi: ts=2 sw=2 tw=120 syntax=php
-// $Id: psummary.php,v 1.62 2003/11/25 18:21:10 thejet Exp $
+// $Id: psummary.php,v 1.63 2004/04/29 23:17:10 paul Exp $
 // Variables Passed in url:
 // id == Participant ID
-include "../etc/config.inc";
-include "../etc/modules.inc";
-include "../etc/project.inc";
-include "../etc/projectstats.php";
-include "../etc/markup.inc";
-include "../etc/participant.php";
+
+include '../etc/config.inc';
+include '../etc/modules.inc';
+include '../etc/project.inc';
+include '../etc/projectstats.php';
+include '../etc/markup.inc';
+include '../etc/participant.php';
 
 function par_list($i, $par, $stats, &$totaltoday, &$totaltotal, $proj_scale, $color_a = "", $color_b = "")
 {
@@ -16,56 +17,53 @@ function par_list($i, $par, $stats, &$totaltoday, &$totaltotal, $proj_scale, $co
     $parid = 0 + $par->get_id();
     $totaltoday += $stats->get_stats_item("work_today");
     $totaltotal += $stats->get_stats_item("work_total");
-    $decimal_places = 0;
     $participant = $par->get_display_name();
 
     ?>
     <tr class="<?=row_background_color($i, $color_a, $color_b)?>">
       <td align="left"><?echo $stats->get_stats_item("overall_rank") . html_rank_arrow($stats->get_stats_item("overall_change")) ?></td>
       <td align="left"><a href="psummary.php?project_id=<?=$gproj->get_id()?>&amp;id=<?=$parid?>"><?=$participant?></a></td>
-      <td align="right"><?echo number_style_convert($stats->get_stats_item("days_working"));
-
-    ?> </td>
+      <td align="right"><?echo number_style_convert($stats->get_stats_item("days_working"));?></td>
       <td align="right"><?echo number_style_convert($stats->get_stats_item("work_total") * $proj_scale) ?> </td>
       <td align="right"><?echo number_style_convert($stats->get_stats_item("work_today") * $proj_scale) ?> </td>
     </tr>
-  <?
-} 
+    <?
+}
+
 function par_footer($totaltoday, $totaltotal, $proj_scale)
 {
     ?>
-  <tr>
-    <td class="tfoot" align="right" colspan="3">Total</td>
-    <td class="tfoot" align="right"><?echo number_style_convert($totaltotal * $proj_scale)?></td>
-    <td class="tfoot" align="right"><?echo number_style_convert($totaltoday * $proj_scale)?></td>
-  </tr>
-<?
-} 
+    <tr>
+      <td class="tfoot" align="right" colspan="3">Total</td>
+      <td class="tfoot" align="right"><?echo number_style_convert($totaltotal * $proj_scale)?></td>
+      <td class="tfoot" align="right"><?echo number_style_convert($totaltoday * $proj_scale)?></td>
+    </tr>
+    <?
+}
+
 // Get the participant's record from STATS_Participant and store it in $person
 $gpart = new Participant($gdb, $gproj, $id);
 if($gpart->get_id() == 0) {
-	$title = "Participant Summary - Error occured ";
-	include "../templates/header.inc";
-	include "../templates/error.inc";
-	include "../templates/footer.inc";
-	exit();
+    $title = "Participant Summary - Error occured ";
+    include "../templates/header.inc";
+    include "../templates/error.inc";
+    include "../templates/footer.inc";
+    exit();
 }
-$gpartstats = new ParticipantStats($gdb, $gproj, $id, null);
-// ###
+
 // Is this person retired?
 if($gpart -> get_retire_to() > 0) {
     header("Location: psummary.php?project_id=$project_id&id=".$gpart -> get_retire_to());
     exit();
-} 
+}
+
+$gpartstats = new ParticipantStats($gdb, $gproj, $id, null);
 
 $title = "Participant Summary for " . $gpart -> get_display_name();
 
-if($gpart -> get_motto() <> "") {
-    $motto = "<i>" . markup_to_html($gpart->get_motto()) . "</i><hr>";
-} 
-
 $lastupdate = last_update('e');
 include "../templates/header.inc";
+
 // Get the participant's best day, store result in $best_day
 /* removed for now - killing sybase
 $qs = "p_phistory @project_id = $project_id, @id = $id, @sort_field = 'WORK_UNITS', @sort_dir = 'desc'";
@@ -83,7 +81,11 @@ $best_rate = number_format((($best_day_units*$constant_keys_in_one_block)/(86400
       <tr>
         <td colspan="3">
           <hr>
-          <? if(isset($motto)) { echo $motto; } ?>
+            <?
+            if($gpart -> get_motto() <> '') {
+                echo '<i>' . markup_to_html($gpart->get_motto()) . '</i><hr>';
+            }
+            ?>
       </td>
       </tr>
       <tr>
@@ -110,7 +112,7 @@ $best_rate = number_format((($best_day_units*$constant_keys_in_one_block)/(86400
         <td align="right">
           <? if ($gpartstats->get_stats_item('days_working') > 0) {
                echo number_style_convert($gpartstats->get_stats_item('work_total') * $gproj->get_scale() / (86400 * $gpartstats->get_stats_item('days_working')), 3);
-             } 
+             }
            ?>
         </td>
         <td align="right">
@@ -119,7 +121,7 @@ $best_rate = number_format((($best_day_units*$constant_keys_in_one_block)/(86400
 ?>
         </td>
       </tr>
-      <!-- 
+      <!--
       <tr>
         <td align="left"><?=$gproj -> get_unscaled_unit_name()?>:</td>
         <td align="right"><?=number_style_convert($gpartstats -> get_stats_item('work_total')) ?></td>
@@ -130,7 +132,7 @@ $best_rate = number_format((($best_day_units*$constant_keys_in_one_block)/(86400
         <td align="right">
           <? if ($gpartstats->get_stats_item('days_working') > 0) {
     number_style_convert($gpartstats->get_stats_item('work_total') / (86400 * $gpartstats->get_stats_item('days_working')), 0);
-} 
+}
 ?>
         </td>
         <td align="right">
@@ -182,22 +184,19 @@ were completed at a rate of <?=$best_rate?> Kkeys/sec.
 */
 ?>
     <p>
+    <? if ( $gpartstats->get_stats_item('work_total') > 0 ) { ?>
+      <img src="graph_phistory.php?project_id=<?=$project_id?>&amp;id=<?=$id?>" /><br>
+    <? } ?>
     <a href="phistory.php?project_id=<?=$project_id?>&amp;id=<?=$id?>">View this Participant's Work Unit Submission History</a>
     </p>
-    <?
-if (($gproj -> get_type() == 'RC5' or $gproj -> get_type() == 'R72') && ($gpartstats -> get_stats_item('work_today') > 0)) {
-    $gprojstats = $gproj->get_current_stats();
-    $odds = number_format($gprojstats->get_stats_item('work_units') / $gpartstats -> get_stats_item('work_today'));
-
-    ?>
-        <p>
-        The odds are 1 in <?=$odds?> that this participant will find the key before anyone else does.
-        </p>
-      <?
-} 
-
-?>
-
+        <? if (($gproj -> get_type() == 'RC5' or $gproj -> get_type() == 'R72') && ($gpartstats -> get_stats_item('work_today') > 0)) {
+            $gprojstats = $gproj->get_current_stats();
+            $odds = number_format($gprojstats->get_stats_item('work_units') / $gpartstats -> get_stats_item('work_today'));
+            ?>
+            <p>
+            The odds are 1 in <?=$odds?> that this participant will find the key before anyone else does.
+            </p>
+        <? } ?>
     <table style="margin:auto;" border="1" cellspacing="0">
       <tr>
         <th class="phead2" colspan="6" align="center">Neighbors</th>
@@ -210,17 +209,17 @@ if (($gproj -> get_type() == 'RC5' or $gproj -> get_type() == 'R72') && ($gparts
         <th class="thead" align="right">Current <?=$gproj->get_scaled_unit_name()?></th>
       </tr>
       <?
-$totaltoday = 0;
-$totaltotal = 0;
-$neighbors = $gpart->get_neighbors();
-$numneighbors = count($neighbors);
-for ($i = 0; $i < $numneighbors; $i++) {
-    if($gpart->get_id() <> $neighbors[$i]->get_id()) {
-        par_list($i, $neighbors[$i], $neighbors[$i]->get_current_stats(), $totaltoday, $totaltotal, $gproj->get_scale());
-    } else {
-        par_list($i, $neighbors[$i], $neighbors[$i]->get_current_stats(), $totaltoday, $totaltotal, $gproj->get_scale(), "row3", "row3");
-    } 
-} 
+        $totaltoday = 0;
+        $totaltotal = 0;
+        $neighbors = $gpart->get_neighbors();
+        $numneighbors = count($neighbors);
+        for ($i = 0; $i < $numneighbors; $i++) {
+            if($gpart->get_id() <> $neighbors[$i]->get_id()) {
+                par_list($i, $neighbors[$i], $neighbors[$i]->get_current_stats(), $totaltoday, $totaltotal, $gproj->get_scale());
+            } else {
+                par_list($i, $neighbors[$i], $neighbors[$i]->get_current_stats(), $totaltoday, $totaltotal, $gproj->get_scale(), "row3", "row3");
+        }
+}
 par_footer($totaltoday, $totaltotal, $gproj->get_scale());
 ?>
 </table>
@@ -252,21 +251,21 @@ if($numfriends >= 1) {
             $printed_self = true;
         }
         par_list($i, $par, $stats, $totaltoday, $totaltotal, $gproj->get_scale());
-    } 
+    }
     par_footer($totaltoday, $totaltotal, $gproj -> get_scale());
     echo("</table>\n");
-} 
+}
 ?>
     <hr>
     <p>
     <form action="ppass.php">
-		<div>
+        <div>
                         <input type="hidden" name="project_id" value="<?=$gproj->get_id()?>">
-			<input type="hidden" name="id" value="<?=$id?>">
-			<input type="submit" value="Please email me my password.">
-		</div>
-	</form>
+            <input type="hidden" name="id" value="<?=$id?>">
+            <input type="submit" value="Please email me my password.">
+        </div>
+    </form>
     </p>
-<?include "../templates/footer.inc";
-
+<?
+include "../templates/footer.inc";
 ?>
