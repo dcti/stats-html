34541c7d66cdbb0b1a34060b5e0c10911376d187
paul <paul>
2004-07-29 10:12:13 +0000

    formatting

--- misc/countries.php
+++ misc/countries.php
@@ -1,21 +1,22 @@
 <?
-# $Id: countries.php,v 1.6 2004/07/16 20:45:27 decibel Exp $
-# vi: ts=2 sw=2 tw=120
+    // $Id: countries.php,v 1.7 2004/07/29 10:12:13 paul Exp $
+    // vi: ts=4 sw=4 tw=120
 
-include "../etc/global.inc";
-include "../etc/modules.inc";
-include "../etc/project.inc";
+    include '../etc/global.inc';
+    include '../etc/modules.inc';
+    include '../etc/project.inc';
 
-$title = "Participating Countries";
-$filename = "../cache/countries_" . $source . "_" . $project_id . ".inc";
+    $title = 'Participating Countries';
+    $filename = '../cache/countries_' . $source . '_' . $project_id . '.inc';
 
-include "../templates/header.inc";
+    include '../templates/header.inc';
 
-if( file_exists($filename)) {
-  readfile($filename);
-} else {
-  display_last_update();
-  trigger_error("Apologies, these pages are currently being built. Please try again in a few minutes",E_USER_ERROR);
-}
+    if( file_exists($filename)) {
+        readfile($filename);
+    } else {
+        display_last_update();
+        trigger_error("Apologies, these pages are currently being built. Please try again in a few minutes",E_USER_ERROR);
+    }
+
+    include '../templates/footer.inc';
 ?>
-<?include "../templates/footer.inc";?>
--- misc/money.php
+++ misc/money.php
@@ -1,29 +1,29 @@
 <?php
+    // $Id: money.php,v 1.8 2004/07/29 10:13:51 paul Exp $
+    // vi: ts=4 sw=4 tw=120
 
-# $Id: money.php,v 1.7 2004/07/16 20:45:27 decibel Exp $
-# vi: ts=2 sw=2 tw=120
+    include '../etc/global.inc';
+    include '../etc/modules.inc';
+    include '../etc/project.inc';
 
-include "../etc/global.inc";
-include "../etc/modules.inc";
-include "../etc/project.inc";
+    $title = 'Disposition of Prize Money';
+    $filename = '../cache/money_' . $project_id . '.inc';
 
-$title = "Disposition of Prize Money";
-$filename = "../cache/money_$project_id.inc";
+    include '../templates/header.inc';
 
-include "../templates/header.inc";
-
-if(file_exists($filename)) {
-  include "$filename";
-} else {
-  display_last_update();
-  ?>
-  <center>
-    <p>
-      Apologies, these pages are currently being built. Please try again in a few minutes.
-    </p>
-  </center>
+    if(file_exists($filename)) {
+        include "$filename";
+    } else {
+        display_last_update();
+        ?>
+        <center>
+        <p>
+        Apologies, these pages are currently being built. Please try again in a few minutes.
+        </p>
+        </center>
   <?
-}
-include "../templates/footer.inc";
+    }
+
+    include '../templates/footer.inc';
 
 ?>
