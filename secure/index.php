a4f42cb829ee94d3fe4afdf7a9e9e2e95f24aee8
fiddles <fiddles>
2005-01-08 01:46:25 +0000

    update the index page

--- secure/index.php
+++ secure/index.php
@@ -1,7 +1,12 @@
 <?php
 include "etc/header.inc";
 ?>
+<p>
 Welcome to stats-slash-secure! Select from the menus above!
+</p>
+<p>
+Note: Only participant management is working at the moment.
+</p>
 <?
 include "etc/footer.inc";
 ?>
