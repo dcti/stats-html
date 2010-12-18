8ddf328cba7dd62235e4b759b38ccae6a15750dc
jlawson <jlawson>
2010-12-18 22:44:21 +0000

    update log viewer for ogr-ng

--- secure/project_logs.php
+++ secure/project_logs.php
@@ -1,14 +1,18 @@
 <?php
 include "etc/header.inc";
 $project_id = $_REQUEST['project_id'];
-if ($project_id == 8) {
-    $log = shell_exec('tail -50 ~statproc/log/r72.log');
-}
-if ($project_id == 24) {
-    $log = shell_exec('tail -50 ~statproc/log/ogrp2.log');
-}
-if ($project_id == 25) {
-    $log = shell_exec('tail -50 ~statproc/log/ogrp2.log');
+switch ($project_id) {
+    case 8:
+        $log = shell_exec('tail -50 ~statproc/log/r72.log');
+	break;
+    case 24:
+    case 25:
+        $log = shell_exec('tail -50 ~statproc/log/ogrp2.log');
+	break;
+    case 26:
+    case 27:
+        $log = shell_exec('tail -50 ~statproc/log/ogrng.log');
+	break;
 }
 echo nl2br($log);
 include "etc/footer.inc";
