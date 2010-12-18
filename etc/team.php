6f925f1c6c6b20a43a35f1048298788a8cb096b4
jlawson <jlawson>
2010-12-18 05:35:58 +0000

    rm extra trailing comma

--- etc/team.php
+++ etc/team.php
@@ -1,5 +1,5 @@
 <?php
-// $Id: team.php,v 1.34 2010/12/18 05:33:48 jlawson Exp $
+// $Id: team.php,v 1.35 2010/12/18 05:35:58 jlawson Exp $
 
 //==========================================
 // file: team.php
@@ -244,14 +244,14 @@ class Team
                            $this->_state->logo,          #6
                            $this->_state->showmembers,   #7
                            $this->_state->showpassword,  #8
-                           (int)$this->_state->listmode, #9
+                           (int)$this->_state->listmode  #9
                            );
            if($this->_state->team != 0)
            {
 		// For updates, we need two additional values.
 		array_push($params,
                            $this->_state->description,   #10
-                           (int)$this->_state->team,     #11
+                           (int)$this->_state->team      #11
 			   );
 	   }
 
