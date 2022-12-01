affb2880ad66060b020bdb5dd37fc21e214f2376
bovine <bovine@distributed.net>
2022-12-01 06:20:02 +0000

    OGR-28 is completd

--- index.php
+++ index.php
@@ -29,7 +29,6 @@
         <div class="col-md-4">
           <h3>Active Projects</h3>
           <p><img src="/images/cowhead.gif" alt="[Cow]"><a href="/projects.php?project_id=8">RC5-72 - RSA Labs' 72bit RC5 Encryption Challenge</a></p>
-          <p><img src="/images/roohead.gif" alt="[Roo]"><a href="/projects.php?project_id=28">OGR-28 - Optimal Golomb Rulers</a></p>
         </div>
         <div class="col-md-4">
           <h3>Completed Projects</h3>
@@ -37,6 +36,7 @@
           <p><img src="/images/roohead.gif" alt="[Roo]"><a href="/projects.php?project_id=25">OGR-25 - Optimal Golomb Rulers</a></p>
           <p><img src="/images/roohead.gif" alt="[Roo]"><a href="/projects.php?project_id=26">OGR-26 - Optimal Golomb Rulers</a></p>
           <p><img src="/images/roohead.gif" alt="[Roo]"><a href="/projects.php?project_id=27">OGR-27 - Optimal Golomb Rulers</a></p>
+          <p><img src="/images/roohead.gif" alt="[Roo]"><a href="/projects.php?project_id=28">OGR-28 - Optimal Golomb Rulers</a></p>
           <p><img src="/images/cowhead.gif" alt="[Cow]"><a href="/projects.php?project_id=3">RC5-56 - RSA Labs' 56bit RC5 Encryption Challenge</a></p>      
           <p><img src="/images/cowhead.gif" alt="[Cow]"><a href="/projects.php?project_id=5">RC5-64 - RSA Labs' 64bit RC5 Encryption Challenge</a></p>
           <p><img src="/images/cowhead.gif" alt="[Cow]"><a href="/projects.php?project_id=205">RC5-64(all) - RSA Labs' 64bit RC5 Encryption Challenge (plus work after key found)</a></p>
